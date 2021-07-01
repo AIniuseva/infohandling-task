813e8fba18600cc740c99471a8cf4da79715061a
AIniuseva <anz.iniu@gmail.com>
2021-07-01 23:08:28 +0300

    Extended text logic implementation

--- src/main/java/com/epam/infohandling/logic/TextLogic.java
+++ src/main/java/com/epam/infohandling/logic/TextLogic.java
@@ -5,6 +5,13 @@ import com.epam.infohandling.model.Component;
 import com.epam.infohandling.model.Composite;
 import com.epam.infohandling.model.Leaf;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
 public class TextLogic {
 
     private final ExpressionCalculator expressionCalculator;
@@ -18,16 +25,152 @@ public class TextLogic {
         this.expressionCalculator = expressionCalculator;
     }
 
-    public Composite calculate(Composite text) {
+    public Composite calculate(Composite text, Map<String, Integer> variables) {
+
+        if(text.getComponents().isEmpty()){
+            return null;
+        }
+
         Composite calculatedComposite = new Composite(text.getComponents());
 
-        processCalculation(calculatedComposite);
+        processCalculation(calculatedComposite, variables);
 
         return calculatedComposite;
     }
 
     public String restore(Composite text) {
         return processRestoration(text, 0);
+    }
+
+    public int countSentencesWithEqualWords(Composite text) {
+
+        if(text.getComponents().isEmpty()){
+            return 0;
+        }
+
+        int count = 0;
+
+        for (Component component : text.getComponents()) {
+            if (component.getChild(0) instanceof Leaf) {
+                count += findEqualWords((Composite) component);
+            } else {
+                count += countSentencesWithEqualWords((Composite) component);
+            }
+        }
+
+        return count;
+    }
+
+    public void sortSentencesLexemeInDescending(Composite text) {
+
+        if(text.getComponents().isEmpty()){
+            return;
+        }
+
+        for (Component component : text.getComponents()) {
+            Collections.sort(((Composite) component).getComponents(), new Comparator<Component>() {
+                @Override
+                public int compare(Component o1, Component o2) {
+                    if (((Composite) o1).getComponents().size() < ((Composite) o2).getComponents().size()) {
+                        return 1;
+                    } else if (((Composite) o1).getComponents().size() > ((Composite) o2).getComponents().size()) {
+                        return -1;
+                    }
+                    return 0; //TODO: change?
+                }
+            });
+        }
+    }
+
+    public void sortInAlphabeticalOrder(Composite text) {
+
+        if(text.getComponents().isEmpty()){
+            return;
+        }
+
+        for (Component component : text.getComponents()) {
+            if (component.getChild(0) instanceof Leaf) {
+                Collections.sort(((Composite) component).getComponents(), new Comparator<Component>() {
+                    @Override
+                    public int compare(Component o1, Component o2) {
+                        return ((Leaf) o1).getValue().compareTo(((Leaf) o2).getValue());
+                    } //TODO: change?
+                });
+            } else {
+                sortInAlphabeticalOrder((Composite) component);
+            }
+        }
+    }
+
+    public void sortByLetterCountInDescending(Composite text) {
+
+        if(text.getComponents().isEmpty()){
+            return;
+        }
+
+        for (Component component : text.getComponents()) {
+            if (component.getChild(0) instanceof Leaf) {
+                Collections.sort(((Composite) component).getComponents(), new Comparator<Component>() {
+                    @Override
+                    public int compare(Component o1, Component o2) {
+                        if (((Leaf) o1).getValue().length() < ((Leaf) o2).getValue().length()) {
+                            return 1;
+                        } else if (((Leaf) o1).getValue().length() > ((Leaf) o2).getValue().length()) {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                });
+            } else {
+                sortByLetterCountInDescending((Composite) component);
+            }
+        }
+    }
+
+    public void removeWordsWithGivenLength(Composite text, int length) {
+
+        if(text.getComponents().isEmpty() || length == 0){
+            return;
+        }
+
+        for (Component component : text.getComponents()) {
+            if (component.getChild(0) instanceof Leaf) {
+                processRemovingByLength((Composite) component, length);
+            } else {
+                removeWordsWithGivenLength((Composite) component, length);
+            }
+        }
+    }
+
+
+    public void removeWordsWithGivenLetter(Composite text, char letter) {
+
+        if(text.getComponents().isEmpty()){
+            return;
+        }
+
+        for (Component component : text.getComponents()) {
+            if (component.getChild(0) instanceof Leaf) {
+                processRemovingByLetter((Composite) component, letter);
+            } else {
+                removeWordsWithGivenLetter((Composite) component, letter);
+            }
+        }
+    }
+
+    public void reverseLexemes(Composite text) {
+
+        if(text.getComponents().isEmpty()){
+            return;
+        }
+
+        for (Component component : text.getComponents()) {
+            if (component.getChild(0) instanceof Leaf) {
+                Collections.reverse(((Composite) component).getComponents());
+            } else {
+                reverseLexemes((Composite) component);
+            }
+        }
     }
 
     private String processRestoration(Composite text, int depth) {
@@ -50,14 +193,64 @@ public class TextLogic {
         return String.valueOf(restoredText);
     }
 
-    private void processCalculation(Composite composite) {
+    private void processCalculation(Composite composite, Map<String, Integer> variables) {
         for (Component component : composite.getComponents()) {
             if (component instanceof Composite) {
-                processCalculation((Composite) component);
+                processCalculation((Composite) component, variables);
             } else if (component instanceof Leaf && ((Leaf) component).getValue().matches(EXPRESSION_PATTERN)) {
-                int calculatedValue = expressionCalculator.calculate(((Leaf) component).getValue());
+                int calculatedValue = expressionCalculator.calculate(((Leaf) component).getValue(), variables);
                 ((Leaf) component).setValue(String.valueOf(calculatedValue));
             }
+        }
+    }
+
+    private int findEqualWords(Composite composite) {
+        if (composite.getChild(0) instanceof Composite) {
+            return 0;
+        }
+        String saveValue = "";
+        int shift = 0;
+        for (int i = 0; i < composite.getComponents().size(); i++) {
+            saveValue = ((Leaf) composite.getComponents().get(i)).getValue();
+            shift++;
+            for (int j = shift; j < composite.getComponents().size(); j++) {
+                String word = ((Leaf) composite.getComponents().get(j)).getValue().replace(",", "")
+                        .toLowerCase(Locale.ROOT);
+                if (saveValue.equals(word)) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private void processRemovingByLength(Composite text, int length) {
+        List<Leaf> wordsToDelete = new ArrayList<>();
+
+        for (Component word : text.getComponents()) {
+            String stringWord = ((Leaf) word).getValue();
+            if (stringWord.length() == length && !stringWord.matches(EXPRESSION_PATTERN)) {
+                wordsToDelete.add((Leaf) word);
+            }
+        }
+
+        for (Leaf leaf : wordsToDelete) {
+            text.remove(leaf);
+        }
+    }
+
+    private void processRemovingByLetter(Composite text, char letter) {
+        List<Leaf> wordsToDelete = new ArrayList<>();
+
+        for (Component word : text.getComponents()) {
+            String stringWord = ((Leaf) word).getValue();
+            if (String.valueOf(stringWord.charAt(0)).equalsIgnoreCase(String.valueOf(letter))) {
+                wordsToDelete.add((Leaf) word);
+            }
+        }
+
+        for (Leaf leaf : wordsToDelete) {
+            text.remove(leaf);
         }
     }
 }
--- src/test/java/com/epam/infohandling/logic/TextLogicTest.java
+++ src/test/java/com/epam/infohandling/logic/TextLogicTest.java
@@ -7,12 +7,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class TextLogicTest {
 
-    private final String TEST_FILE_PATH = "src/test/resources/textSample.txt";
+    private static final String TEST_FILE_PATH = "src/test/resources/textSample.txt";
     private TextLogic textLogic;
     private TestData testData;
 
@@ -26,18 +28,30 @@ public class TextLogicTest {
         //when
         ExpressionCalculator expressionCalculator = mock(ExpressionCalculator.class);
         textLogic = new TextLogic(expressionCalculator);
+        Map<String, Integer> variables = testData.getVariables();
 
-        when(expressionCalculator.calculate("[13 2 +]")).thenReturn(15);
-        when(expressionCalculator.calculate("[3 5 +]")).thenReturn(8);
-        when(expressionCalculator.calculate("[3 4 – 9 * 6 +]")).thenReturn(-3);
-        when(expressionCalculator.calculate("[ 20 1 - ]")).thenReturn(19);
-        when(expressionCalculator.calculate("[71 2 * 3 +]")).thenReturn(145);
-        when(expressionCalculator.calculate("[5 8 + 120 *]")).thenReturn(1560);
+        when(expressionCalculator.calculate("[13 x +]", variables)).thenReturn(15);
+        when(expressionCalculator.calculate("[3 5 +]", variables)).thenReturn(8);
+        when(expressionCalculator.calculate("[8 9 + 2 *]", variables)).thenReturn(34);
+        when(expressionCalculator.calculate("[3 4 – 9 * 6 +]", variables)).thenReturn(-3);
+        when(expressionCalculator.calculate("[ 20 1 - ]", variables)).thenReturn(19);
+        when(expressionCalculator.calculate("[6 2 /]", variables)).thenReturn(3);
+        when(expressionCalculator.calculate("[71 2 * 3 +]", variables)).thenReturn(145);
+        when(expressionCalculator.calculate("[5 y + 120 *]", variables)).thenReturn(1560);
+        when(expressionCalculator.calculate("[80 2 / + 3]", variables)).thenReturn(43);
 
-        Composite actualComposite = textLogic.calculate(testData.getCompositeTextTree());
-        Composite expectedComposite = testData.getCalculatedCompositeTextTree();
-        //then
-        Assert.assertEquals(expectedComposite, actualComposite);
+        Composite actualComposite = textLogic.calculate(testData.getSimpleComposite(), variables);
+        Composite expectedComposite = testData.getCalculatedComposite();
+
+        //Assert.assertEquals(expectedComposite, actualComposite);
+
+        Assert.assertEquals(3, actualComposite.getComponents().size());
+        Assert.assertEquals(((Composite) expectedComposite.getChild(0).getChild(0)).getComponents(),
+                ((Composite) actualComposite.getChild(0).getChild(0)).getComponents());
+        Assert.assertEquals(((Composite) expectedComposite.getChild(1).getChild(0)).getComponents(),
+                ((Composite) actualComposite.getChild(1).getChild(0)).getComponents());
+        Assert.assertEquals(((Composite) expectedComposite.getChild(2).getChild(0)).getComponents(),
+                ((Composite) actualComposite.getChild(2).getChild(0)).getComponents());
     }
 
     @Test
@@ -49,5 +63,93 @@ public class TextLogicTest {
         String expectedText = testData.getTextForTest(TEST_FILE_PATH);
         //then
         Assert.assertEquals(expectedText, actualText);
+    }
+
+    @Test
+    public void testCountSentencesWithEqualWords() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        int actualResult = textLogic.countSentencesWithEqualWords(testData.getCompositeTextTree());
+        //when
+        Assert.assertEquals(4, actualResult);
+    }
+
+    @Test
+    public void testReverseLexemes() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        Composite actualComposite = testData.getSimpleComposite();
+        textLogic.reverseLexemes(actualComposite);
+
+        Composite expectedComposite = testData.getReverseComposite();
+        //then
+        Assert.assertEquals(expectedComposite, actualComposite);
+    }
+
+    @Test
+    public void testRemovingWithGivenLength() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        Composite actualComposite = testData.getSimpleComposite();
+        textLogic.removeWordsWithGivenLength(actualComposite, 4);
+
+        Composite expectedComposite = testData.getCompositeWithRemovedWords();
+        //then
+        Assert.assertEquals(expectedComposite, actualComposite);
+    }
+
+    @Test
+    public void testRemovingWithGivenLetter() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        Composite actualComposite = testData.getSimpleComposite();
+        textLogic.removeWordsWithGivenLetter(actualComposite, 'w');
+
+        Composite expectedComposite = testData.getCompositeWithRemovedWords();
+        //then
+        Assert.assertEquals(expectedComposite, actualComposite);
+    }
+
+    @Test
+    public void testSortInAlphabeticalOrder() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        Composite actualComposite = testData.getCompositeWithWordsOnly();
+        textLogic.sortInAlphabeticalOrder(actualComposite);
+
+        Composite expectedComposite = testData.getAlphabeticallySortedComposite();
+        //then
+        Assert.assertEquals(expectedComposite, actualComposite);
+    }
+
+    @Test
+    public void testSortSentencesLexemeInDescending() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        Composite actualComposite = testData.getCompositeWithWordsOnly();
+        textLogic.sortSentencesLexemeInDescending(actualComposite);
+
+        Composite expectedComposite = testData.getSortedSentencesInDescending();
+        //then
+        Assert.assertEquals(expectedComposite, actualComposite);
+    }
+
+    @Test
+    public void testSortByLetterCountInDescending() {
+        //given
+        textLogic = new TextLogic();
+        //when
+        Composite actualComposite = testData.getCompositeWithWordsOnly();
+        textLogic.sortByLetterCountInDescending(actualComposite);
+
+        Composite expectedComposite = testData.getSortedByLetterCountInDescending();
+        //then
+        Assert.assertEquals(expectedComposite, actualComposite);
     }
 }
